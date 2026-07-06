0c3381a861a0d7c565f1d84cb93a7d3acec646cc
jakejunior-tech <jakejunior2008@gmail.com>
2026-07-06 16:50:53 +0100

    Embed Firebase config directly in JS files to fix admin login

--- admin/admin.js
+++ admin/admin.js
@@ -1,3 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDAqUbYPX5wVpRn1Tgs4uquB_qhMAUz8q0",
+  authDomain: "logic-gadget-store.firebaseapp.com",
+  projectId: "logic-gadget-store",
+  storageBucket: "logic-gadget-store.firebasestorage.app",
+  messagingSenderId: "914698370044",
+  appId: "1:914698370044:web:a5bc33e15a76094a68d079"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
 let currentCategory = "iphones";
 let allProducts = {};
 
